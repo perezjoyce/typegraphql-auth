4bea5152b329f7e768fc8f47ef2bf1e9f5c1303f
perez.joyce <japerez.ph@gmail.com>
2020-09-29 11:22:45 +0800

    db setup

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import "reflect-metadata";
 import { ApolloServer } from 'apollo-server-express'
 import * as Express from 'express'
 import { buildSchema, Resolver, Query } from 'type-graphql'
+import { createConnection } from "typeorm";
 const PORT = 4000;
 
 @Resolver()
 class HelloResolver {
-    @Query(() => String, { name: "helloWorld" })
+    @Query(() => String, { name: "helloWorld", nullable: true, description: "sample description" })
     async hello() {
         return "Hello, World!"
     }
 }
 
 const main = async () => {
+    await createConnection();
+
     const schema = await buildSchema({
         resolvers: [HelloResolver],
     })
