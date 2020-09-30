3a452df63cd4af387c6fb00ea644f351395034e4
perez.joyce <japerez.ph@gmail.com>
2020-09-30 21:42:11 +0800

    test register resolver

--- src/modules/user/register/Register.test.ts
+++ src/modules/user/register/Register.test.ts
@@ -1,13 +1,20 @@
 import { Connection } from "typeorm";
+import faker from 'faker';
 
 import { testConn } from "../../../test-utils/testConn";
 import { gCall } from "../../../test-utils/gCall";
+import { redis } from "../../../redis";
+import { User } from "../../../entity/User";
 
 let conn: Connection;
 beforeAll(async () => {
     conn = await testConn();
+    if (redis.status == "end") {
+        await redis.connect();
+    }
 });
 afterAll(async () => {
+    redis.disconnect();
     await conn.close();
 });
 
@@ -27,18 +34,38 @@ mutation Register($data: RegisterInput!) {
 
 describe("Register", () => {
     it("create user", async () => {
-        console.log(
-            await gCall({
-                source: registerMutation,
-                variableValues: {
-                    data: {
-                        firstName: "bob",
-                        lastName: "bob2",
-                        email: "bob@bob.com",
-                        password: "asdfasdf"
-                    }
+        const user = {
+            firstName: faker.name.findName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password()
+        }
+
+        console.log("PASSWORD", user.password)
+        console.log("EMAIL", user.email)
+
+
+        const response = await gCall({
+            source: registerMutation,
+            variableValues: {
+                data: user
+            }
+        })
+
+        expect(response).toMatchObject({
+            data: {
+                register: {
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email
                 }
-            })
-        );
+            }
+        })
+
+        //check if user was saved in the db
+        const dbUser = await User.findOne({ where: { email: user.email } })
+        expect(dbUser).toBeDefined()
+        expect(dbUser!.confirmed).toBeFalsy()
+        expect(dbUser!.firstName).toBe(user.firstName)
     });
 });
